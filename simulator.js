4c9f1e83bcb4e76528c3e11d7bb76d3b54717c62
NimuKaT <takumin923@gmail.com>
2016-07-26 12:58:52 +1000

    Fixed miss use of const - removed var after const - added main function
    26/07/16 12:30 ~ 13:00 Takumi

--- simulator.js
+++ simulator.js
@@ -1,3 +1,9 @@
+const UNIVERSAL_GRAVITATIONAL_CONSTANT = 6.67e-11;
+const KM_TO_PIXELS = 1/1e3; //subject to change
+
+var magnificationMultiplyer = 1.0;
+var currentCoordinate = 1;
+
 var openTutorialWindow = function() {
   document.getElementById('tutorial').className = 'open';
 };
@@ -42,17 +48,28 @@ var init = function() {
   });
 };
 
-
-init();
-
-
-
-function planet (density=1, radius=1, color=#ffffff, x=0, y=0){
+function planet (density=1, radius=1, color='#ffffff', x=0, y=0){
     this.density = density;
     this.radius = radius;
     this.color = color;
     this.x = x;
     this.y = y;
     this.mass = (4/3) * math.PI * math.pow(this.radius, 3);
-     
+
+};
+
+function main(){
+    this.objects = [];
+
+    this.createObject = function(){
+        this.object.append(planet());
+    }
+    this.update = function(){
+
+    };
+
 }
+
+
+
+init();
