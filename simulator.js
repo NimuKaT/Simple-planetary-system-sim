0150c6c6ef63074dd7971641e4536c36b2ecdad7
NimuKaT <takumin923@gmail.com>
2016-07-26 13:29:45 +1000

    Implemented basic Hit detection - Added hitDetect function - Moved gloable variable into main function - Added basic object creation 26/07/16 13:00 ~ 13:30 Takumi

--- simulator.js
+++ simulator.js
@@ -1,8 +1,6 @@
 const UNIVERSAL_GRAVITATIONAL_CONSTANT = 6.67e-11;
 const KM_TO_PIXELS = 1/1e3; //subject to change
-
-var magnificationMultiplyer = 1.0;
-var currentCoordinate = 1;
+const COLLISION_THRESHOLD = 0.85;
 
 var openTutorialWindow = function() {
   document.getElementById('tutorial').className = 'open';
@@ -48,27 +46,48 @@ var init = function() {
   });
 };
 
-function planet (density=1, radius=1, color='#ffffff', x=0, y=0){
+function planet (density, radius, color, x, y){
     this.density = density;
     this.radius = radius;
     this.color = color;
     this.x = x;
     this.y = y;
-    this.mass = (4/3) * math.PI * math.pow(this.radius, 3);
+    this.volume = (4/3) * Math.PI * Math.pow(this.radius, 3);
 
 };
 
 function main(){
     this.objects = [];
+    this.magnificationMultiplyer = 1.0;
+    this.currentCoordinate = 1;
 
-    this.createObject = function(){
-        this.object.append(planet());
-    }
+    this.createObject = function(density, radius, color, x, y){
+        this.objects.push(planet(density, radius, color, x, y));
+    };
+
     this.update = function(){
+        for(var i = 0; i < this.object.length; i++){
+
+        };
 
     };
 
-}
+    this.hitDetect = function(object_1, object_2){
+        var hasHit = false;
+        var x1 = object_1.x;
+        var y1 = object_1.y;
+        var r1 = object_1.radius;
+        var x2 = object_2.x;
+        var y2 = object_2.y;
+        var r2 = object_2.radius;
+        var distance = Math.root((x1 - x2)^2 + (y1 - y1)^2);
+        if (distance >= COLLISION_THRESHOLD*(r1 + r2)){
+            hasHit = true;
+        };
+        return hasHit;
+    };
+
+};
 
 
 
